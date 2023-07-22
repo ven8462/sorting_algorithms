b359ca5eb884777fb47f92ba4c7c07cc6bc15dfd
lavender <lavenderanyango9@gmail.com>
2023-07-22 17:34:28 +0300

    fixing bugs

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,30 +11,32 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t a, b;
 	int tmp;
-    int swapped;
+	int swapped;
 
-	if (size == 0)
-		return;
-	/*loop through to get the elements*/
-	for (a = 0; a < size - a - 1; a++)
+	if (size < 2 || array == NULL)
 	{
+		return;
+	}
+	/*loop through to get the elements*/
+	for (a = 0; a < size - 1; a++)
+	{
+		swapped = 0;
 		/* loop to compare*/
-		for (b = 0; b < size - 1 ; b++)
+		for (b = 0; b < size - a - 1 ; b++)
 		{
-            swapped = 0;
 			/* if the 1st index is greater than the second swap*/
 			if (array[b] > array[b + 1])
 			{
 				tmp = array[b];
 				array[b] = array[b + 1];
 				array[b + 1] = tmp;
-                swapped = 1;
+				swapped = 1;
 				print_array(array, size);/* print after each swap*/
 			}
 		}
-        if (swapped == 0)
-        {
-            return;
-        }
+		if (swapped == 0)
+		{
+			return;
+		}
 	}
 }
